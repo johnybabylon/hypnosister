987f58a69723ea16b5a0d93455972e7295e74437
johny babylon <ztredded@gmail.com>
2015-11-02 03:00:48 -0800

    Initial Commit

---
+++ Hypnosister/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Hypnosister
+//
+//  Created by elliott chavis on 11/2/15.
+//  Copyright (c) 2015 elliott chavis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
